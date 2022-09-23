f86aa23878d910f80ea9970c596c00db8d5ad2a4
Irina Rusinova <rusinovarusya@mail.ru>
2022-09-23 12:03:58 +0300

    fix: fix array out of daylight bounds

--- src/components/daylight-indicator/use-daylight-indicator-data.ts
+++ src/components/daylight-indicator/use-daylight-indicator-data.ts
@@ -2,8 +2,7 @@ const sunrise = 1646306882;
 const sunset = 1646347929;
 const currentTime = 1646318698;
 
-
-const elapsedPartOfDaylight = (sunset - currentTime) / (sunset - sunrise) * 100;
+const elapsedPartOfDaylight = (sunrise < currentTime) ? (currentTime - sunrise) / (sunset - sunrise) * 100 : 0;
 const restOfDaylight = (elapsedPartOfDaylight) ? elapsedPartOfDaylight + 5 : elapsedPartOfDaylight;
 
 export const gradient = {
