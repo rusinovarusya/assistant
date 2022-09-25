ac222be77d4c380e5edc13e6efec0f5bdf4e53ac
Irina Rusinova <rusinovarusya@mail.ru>
2022-09-25 14:42:10 +0300

    feat: run daylight indicator

--- src/components/daylight-indicator/DaylightIndicator.tsx
+++ src/components/daylight-indicator/DaylightIndicator.tsx
@@ -1,9 +1,18 @@
 import { FC, memo } from "react";
 import styles from "./DaylightIndicator.module.css";
-import { gradient } from "./use-daylight-indicator-data";
+import { getGradient, useDaylightIndicatorData } from "./use-daylight-indicator-data";
 
 
 const DaylightIndicator: FC = () => {
+  const { daylightHours, isLoading } = useDaylightIndicatorData();
+  const gradient = getGradient(daylightHours.sunrise, daylightHours.sunset, daylightHours.currentTime);
+  
+  if (isLoading) {
+    return (
+      <div className={styles.indicator}></div>
+    )
+  }
+
   return (
     <div className={styles.indicator} style={gradient}></div>
   )
--- src/components/daylight-indicator/use-daylight-indicator-data.ts
+++ src/components/daylight-indicator/use-daylight-indicator-data.ts
@@ -1,10 +1,37 @@
-const sunrise = 1646306882;
-const sunset = 1646347929;
-const currentTime = 1646318698;
+import { useGetWeatherQuery } from "../../features/weatherApi";
 
-const elapsedPartOfDaylight = (sunrise < currentTime) ? (currentTime - sunrise) / (sunset - sunrise) * 100 : 0;
-const restOfDaylight = (elapsedPartOfDaylight) ? elapsedPartOfDaylight + 5 : elapsedPartOfDaylight;
 
-export const gradient = {
-  background: `linear-gradient(to right, var(--main-color) ${elapsedPartOfDaylight}%, var(--extra-color) ${restOfDaylight}%)`
+export type DaylightHours = {
+  sunrise?: number;
+  sunset?: number;
+  currentTime?: number;
+}
+
+export const useDaylightIndicatorData = (): {
+  daylightHours: DaylightHours,
+  isLoading: boolean
+} => {
+  const lat: number = 0;
+  const lon: number = 0;
+  const language = "en";
+
+  const { data, isLoading } = useGetWeatherQuery({ lat, lon, language });
+
+  const daylightHours = {
+    sunrise: data?.current.sunrise,
+    sunset: data?.current.sunset,
+    currentTime: data?.current.dt
+  }
+  return { daylightHours, isLoading };
+}
+
+
+export const getGradient = (sunrise?: number, sunset?: number, currentTime?: number) => {
+  if (sunrise && sunset && currentTime) {
+    const elapsedPartOfDaylight = (sunrise < currentTime) ? (currentTime - sunrise) / (sunset - sunrise) * 100 : 0;
+    const restOfDaylight = (elapsedPartOfDaylight) ? elapsedPartOfDaylight + 5 : elapsedPartOfDaylight;
+    return { 
+      background: `linear-gradient(to right, var(--main-color) ${elapsedPartOfDaylight}%, var(--extra-color) ${restOfDaylight}%)` 
+    }
+  }
 }
